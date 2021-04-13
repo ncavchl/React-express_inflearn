db39eab26344932d34fa7940a750d262622a8659
ncavchl <ncavchl@hyundai-autoever.com>
2021-04-14 01:19:21 +0900

    setting

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          몽총이
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          몽총이
         </a>
       </header>
     </div>
