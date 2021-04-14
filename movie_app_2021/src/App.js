0dd83623a05e8e22d491e47d03c3240ee5ad82c8
ncavchl <ncavchl@hyundai-autoever.com>
2021-04-14 14:56:31 +0900

    dynamic data - function 이용해서 출력

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -30,12 +30,15 @@ const foodILike = [
 
 ];
 
+function renderFood(dish){
+  return <Food name={dish.name} picture={dish.image} />
+}
+
 function App() {
   return (
     <div>
-  {foodILike.map( (dish, index) => (
-    <Food key={index} name={dish.name} picture={dish.image}/>
-    ))}
+      {console.log(foodILike.map(renderFood))}
+      {foodILike.map( renderFood )}
     </div>
 
   );
