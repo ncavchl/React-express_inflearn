32b7212ca542fd0dca7029c53bab27c9efcbd1ea
ncavchl <ncavchl@hyundai-autoever.com>
2021-04-14 15:01:21 +0900

    dynamic data - props

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -1,28 +1,21 @@
 import React from 'react';
 
-function Food({key, name, picture}) {
-  return (
-  <div>
-    <h1>I like {name} + {key}</h1>
-    <img src={picture}/>
-  </div>);
-  
-}
+
 const foodILike = [
   {
-    key:'1',
+    id:'1',
     name: "Orange",
     image:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.allfreschgroup.com%2Fwp-content%2Fuploads%2F2017%2F01%2FValencia-Orange.png&f=1&nofb=1",
   },
   {
-    key:'2',
+    id:'2',
     name: "1222",
     image:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.allfreschgroup.com%2Fwp-content%2Fuploads%2F2017%2F01%2FValencia-Orange.png&f=1&nofb=1",
   },
   {
-    key:'3',
+    id:'3',
     name: "3333",
     image:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.allfreschgroup.com%2Fwp-content%2Fuploads%2F2017%2F01%2FValencia-Orange.png&f=1&nofb=1",
@@ -30,16 +23,22 @@ const foodILike = [
 
 ];
 
-function renderFood(dish){
-  return <Food name={dish.name} picture={dish.image} />
+function Food({name, picture}) {
+  return (
+  <div>
+    <h1>I like f {name}</h1>
+    <img src={picture} alt={name}/>
+  </div>);
+  
 }
 
 function App() {
   return (
     <div>
-      {console.log(foodILike.map(renderFood))}
-      {foodILike.map( renderFood )}
-    </div>
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={true} />
+      ))}
+      </div>
 
   );
 }
