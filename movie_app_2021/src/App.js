ec05d079e34169114b3b231760da62f153547678
ncavchl <ncavchl@hyundai-autoever.com>
2021-04-14 14:00:11 +0900

    props example

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({fav}) {
+  return <h1>I like {fav}</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          몽총이
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          몽총이
-        </a>
-      </header>
+    <div>
+      <h1>main</h1>
+
+      <Food fav="kimchi" />
+      <Food fav="aaabb" />
+      <Food fav="smsm" />
+      <Food fav="long" />
     </div>
+
   );
 }
 
