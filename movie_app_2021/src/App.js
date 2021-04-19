1a4db37d7fc26d1b3907c2983fe9e9f8690d46dc
ncavchl <ncavchl@hyundai-autoever.com>
2021-04-19 13:38:42 +0900

    mount update unmount

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types'; 
 
 class App extends React.Component{
+  // counstructor(props) {
+  //   super(props);
+  //   console.log("hello");
+  // }
+
   state = {
     count:0
   };
@@ -11,8 +16,18 @@ class App extends React.Component{
   minus= () => {
     this.setState(current => ({count: current.count - 1}));
   };
+  componentDidMount(){
+    console.log("component rendered");
+  }
+  componentDidUpdate(){
+    console.log("component update");
+  }
+  componentWillUnmount(){
+    console.log("gootbye");
+  }
 
   render (){
+    console.log("render");
     return (
     <div>
       <h1>The number is: {this.state.count}</h1>
