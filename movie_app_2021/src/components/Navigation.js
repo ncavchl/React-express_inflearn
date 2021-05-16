a14f7976edbfdaf3ce368e30d4acf9e58c5b5c29
ncavchl <ncavchl@ajou.ac.kr>
2021-05-16 18:36:20 +0900

    design navigation

--- movie_app_2021/src/components/Navigation.js
+++ movie_app_2021/src/components/Navigation.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link }  from "react-router-dom";
+import "./Navigation.css";
 
 function Navigation(){
-    return <div>
+    return <div className="navigation">
         <Link to="/">home</Link>
         <Link to="/about">about</Link>
     </div>;
