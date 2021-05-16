d06f04679cd2909ccc3f6710425dd42044282ca4
ncavchl <ncavchl@ajou.ac.kr>
2021-05-16 18:28:26 +0900

    routing test

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { HashRouter, Route } from "react-router-dom";
 import home from "./routes/home";
 import About from "./routes/About";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
 function App(){
-  return 
-  <HashRouter>
-    <Route path="/" component={home}/>
+  return <HashRouter>
+    <Navigation />
+    <Route path="/" exact={true} component={home}></Route>
     <Route path="/about" component={About}/>
   </HashRouter>;
 }
