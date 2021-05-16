638572df2d8db81a2a2ceca67003681af68ac6af
ncavchl <ncavchl@ajou.ac.kr>
2021-05-16 22:31:53 +0900

    handling - movie detail

--- movie_app_2021/src/routes/Detail.js
+++ movie_app_2021/src/routes/Detail.js
@@ -1,8 +1,18 @@
 import React from "react";
 
-function Detail(props){
-    console.log(props);
-    return <span>hello</span>;
+class Detail extends React.Component{
+    componentDidMount(){
+        console.log(this.props);
+
+        const {location, history} = this.props;
+        if(location.state === undefined){
+            history.push("/");
+        }
+    }
+    render(){
+        return <span>hello</span>;
+    }
 }
+
 
 export default Detail;
